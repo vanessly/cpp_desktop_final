5610367d007816d296ef1eee1e13dcdd8fd7830f
CC Ly <vanessly@umich.edu>
2024-04-05 16:18:10 -0400

    Finished

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 // Light and Dark Mode Toggle
 const modeToggle = document.getElementById('mode-toggle');
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "light-mode") {
+    document.body.classList.remove('dark-mode');
+} else {
+    document.body.classList.add('dark-mode');
+}
+
 modeToggle.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
     if (document.body.classList.contains('dark-mode')) {
         modeToggle.textContent = 'Toggle Light Mode';
+        localStorage.setItem("theme", "dark-mode");
     } else {
         modeToggle.textContent = 'Toggle Dark Mode';
+        localStorage.setItem("theme", "light-mode");
     }
 });
 
@@ -23,3 +33,6 @@ navToggle.addEventListener('click', function() {
     document.querySelector('nav > ul').classList.toggle('show');
 });
 
+if (localStorage.getItem('lightMode') === 'enabled') {
+    document.body.classList.add('light-mode')
+  }
